3f703465ab587899a77f4978d875be3bb09a351c
Bevet2 <98226545+Bevet2@users.noreply.github.com>
2025-04-28 10:32:06 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -12,5 +12,5 @@ def data():
     return send_from_directory('.', 'data_articles.json')
 
 if __name__ == '__main__':
-    # Par défaut sur http://localhost:5000
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # <- Prendre le port imposé par Railway
+    app.run(host='0.0.0.0', port=port, debug=True)  # <- Écouter toutes les adresses (0.0.0.0)
