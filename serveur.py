ecdb46d8642efa3f54cbade97595d500fde1819c
Bevet2 <98226545+Bevet2@users.noreply.github.com>
2025-04-28 10:36:38 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -3,14 +3,17 @@ import os
 
 app = Flask(__name__)
 
+# Le chemin absolu du dossier où est ce script (serveur.py)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 @app.route('/')
 def index():
-    return send_from_directory('.', 'schema.html')
+    return send_from_directory(BASE_DIR, 'schema.html')
 
 @app.route('/data_articles.json')
 def data():
-    return send_from_directory('.', 'data_articles.json')
+    return send_from_directory(BASE_DIR, 'data_articles.json')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # <- Prendre le port imposé par Railway
-    app.run(host='0.0.0.0', port=port, debug=True)  # <- Écouter toutes les adresses (0.0.0.0)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
